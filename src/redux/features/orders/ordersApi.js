0d47d2adb95f90008653bc503f97b9e45e21bc34
Dattaram Shinde <dattasshinde16@gmail.com>
2026-03-23 13:05:54 +0530

    final api fix

--- src/redux/features/orders/ordersApi.js
+++ src/redux/features/orders/ordersApi.js
@@ -1,24 +1,63 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import getBaseUrl from "../../../utils/baseURL";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import getBaseUrl from "../../../utils/baseURL";
 
+
+// const ordersApi = createApi({
+//     reducerPath: 'ordersApi',
+//     baseQuery: fetchBaseQuery({
+//         baseUrl: `${getBaseUrl()}/api/orders`,
+//         credentials: 'include'
+//     }),
+//     tagTypes: ['Orders'],
+//     endpoints: (builder) => ({
+//         createOrder: (builder.mutation) ({
+//             query: (newOrder) => ({
+//                 url: "/",
+//                 method: "POST",
+//                 body: newOrder,
+//                 credentials: 'include',
+//             })
+//         }),
+//         getOrderByEmail: (builder.query) ({
+//             query: (email) => ({
+//                 url: `/email/${email}`
+//             }),
+//             providesTags: ['Orders']
+//         })
+//     })
+// })
+
+// export const {useCreateOrderMutation, useGetOrderByEmailQuery} = ordersApi;
+
+// export default ordersApi;
+
+
+
+
+
+
+
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseUrl = "https://book-app-backend-blush.vercel.app";
 
 const ordersApi = createApi({
     reducerPath: 'ordersApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: `${getBaseUrl()}/api/orders`,
+        baseUrl: `${baseUrl}/api/orders`,
         credentials: 'include'
     }),
     tagTypes: ['Orders'],
     endpoints: (builder) => ({
-        createOrder: (builder.mutation) ({
+        createOrder: builder.mutation({
             query: (newOrder) => ({
                 url: "/",
                 method: "POST",
                 body: newOrder,
-                credentials: 'include',
             })
         }),
-        getOrderByEmail: (builder.query) ({
+        getOrderByEmail: builder.query({
             query: (email) => ({
                 url: `/email/${email}`
             }),
@@ -27,6 +66,6 @@ const ordersApi = createApi({
     })
 })
 
-export const {useCreateOrderMutation, useGetOrderByEmailQuery} = ordersApi;
+export const { useCreateOrderMutation, useGetOrderByEmailQuery } = ordersApi;
 
 export default ordersApi;
